7576f3f71705f7a9d6015e087bfae96f4899b2e3
Chi Huỳnh <hthchi2001@gmai.com>
2026-04-29 17:05:57 +0700

    chore: update table name

---
+++ src/FashionStore.Infrastructure/Persistence/Migrations/20260429100424_UpdateFileUploadName.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FashionStore.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFileUploadName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FileUpload",
+                table: "FileUpload");
+
+            migrationBuilder.RenameTable(
+                name: "FileUpload",
+                newName: "FileUploads");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FileUploads",
+                table: "FileUploads",
+                column: "FileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FileUploads",
+                table: "FileUploads");
+
+            migrationBuilder.RenameTable(
+                name: "FileUploads",
+                newName: "FileUpload");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FileUpload",
+                table: "FileUpload",
+                column: "FileId");
+        }
+    }
+}
